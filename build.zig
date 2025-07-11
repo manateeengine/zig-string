87090cee2d5e7483e4dbf2c5fbc9cd19698daad4
James Kienle <james.kienle@gmail.com>
2025-07-11 15:02:21 -0700

    More logical build.zig ordering

--- build.zig
+++ build.zig
@@ -4,19 +4,20 @@ const build_module = @import("src/build/main.zig");
 pub fn build(b: *std.Build) !void {
     // Build Setup
     const build_config = build_module.BuildConfig.init(b);
-    const check_step = b.step("check", "Check if foo compiles");
 
     // Module
     const zig_string_module = build_module.ZigStringModule.init(b, &build_config);
 
-    // Tests
+    // Test Step
     const tests_exe = build_module.TestsExe.init(b, &build_config, &zig_string_module);
-    tests_exe.addRunStep();
     tests_exe.addTestStep();
 
-    // Examples
+    // Example Steps
     const basic_example_exe = build_module.ExampleExe.init(b, &build_config, &zig_string_module, "basic");
     basic_example_exe.install();
     try basic_example_exe.addRunStep();
-    basic_example_exe.addToCheckStep(check_step);
+
+    // Check Step
+    const check_step = b.step("check", "Check if the project compiles");
+    basic_example_exe.addToCheckStep(check_step); // Ensure examples compile
 }
