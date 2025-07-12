9ca06f29772806efc4c6e5985cff15bdad74f590
James Kienle <james.kienle@gmail.com>
2025-07-11 17:18:28 -0700

    Improve Build System Utils

--- build.zig
+++ build.zig
@@ -5,20 +5,26 @@ pub fn build(b: *std.Build) !void {
     // Build Setup
     const build_config = build_module.BuildConfig.init(b);
 
-    // Module
+    // Module Setup
     const zig_string_module = build_module.ZigStringModule.init(b, &build_config);
 
-    // Test Step
-    const tests_exe = build_module.TestsExe.init(b, &build_config, &zig_string_module);
-    tests_exe.addTestStep();
-
-    // Example Steps
+    // Examples Setup
     const basic_example_exe = build_module.ExampleExe.init(b, &build_config, &zig_string_module, "basic");
-    basic_example_exe.install();
-    try basic_example_exe.addRunStep();
+
+    // Test Setup
+    const tests_exe = build_module.TestsExe.init(b, &build_config, &zig_string_module);
 
     // Check Step
     const check_step = b.step("check", "Check if the project compiles");
     basic_example_exe.addToCheckStep(check_step); // Ensure examples compile
     tests_exe.addToCheckStep(check_step); // Required since example doesn't touch all code paths
+
+    // Install Step
+    try basic_example_exe.addToInstallStep();
+
+    // Run Example Steps
+    try basic_example_exe.addRunExampleStep();
+
+    // Test Step
+    tests_exe.addTestStep();
 }
--- src/build/ExampleExe.zig
+++ src/build/ExampleExe.zig
@@ -1,4 +1,4 @@
-//! A struct that creates and configures an exe used to run zig-string's examples.
+//! A struct that creates and configures an exe used to run a zig-string's example.
 
 const std = @import("std");
 const BuildConfig = @import("BuildConfig.zig");
@@ -6,22 +6,15 @@ const ZigStringModule = @import("ZigStringModule.zig");
 
 const ExampleExe = @This();
 
-/// The executable for all zig-string examples.
+/// The executable for the examples.
 exe: *std.Build.Step.Compile,
-/// A special, non-installed copy of the example to be used with the "check" step.
-///
-/// ## Remarks
-/// This is used by the ZLS "Build on Save" functionality to get better in-editor visibility into
-/// compilation errors. For more information, see https://zigtools.org/zls/guides/build-on-save.
-exe_check: *std.Build.Step.Compile,
-/// The install step for the zig-string examples executable.
-install_step: *std.Build.Step.InstallArtifact,
 /// The name of the example to build. This should be 1:1 with a file name in the `examples` dir
 /// with the `.zig` extension omitted.
 name: []const u8,
 
 /// Initializes a new ExampleExe struct with the provided name, BuildConfig, and ZigStringModule.
 ///
+/// ## Remarks
 /// This also adds the provided ZigStringModule as an import with the alias "string" to the newly
 /// initialized exe.
 pub fn init(b: *std.Build, build_config: *const BuildConfig, module: *const ZigStringModule, name: []const u8) ExampleExe {
@@ -32,48 +25,37 @@ pub fn init(b: *std.Build, build_config: *const BuildConfig, module: *const ZigS
         .target = build_config.target,
     });
     examples_exe.root_module.addImport("string", module.module);
-    const examples_exe_check = b.addExecutable(.{
-        .name = name,
-        .optimize = build_config.optimize,
-        .root_source_file = b.path(b.pathJoin(&.{ "examples", name, "main.zig" })),
-        .target = build_config.target,
-    });
-    examples_exe_check.root_module.addImport("string", module.module);
-
-    const examples_install_step = b.addInstallArtifact(examples_exe, .{});
 
     return ExampleExe{
         .exe = examples_exe,
-        .exe_check = examples_exe_check,
-        .install_step = examples_install_step,
         .name = name,
     };
 }
 
-/// Adds a build command and step, allowing the exe to be run via "zig build example".
-pub fn addRunStep(self: *const ExampleExe) !void {
+/// Adds a run command and step, allowing the exe to be run via "zig build example-{name}".
+pub fn addRunExampleStep(self: *const ExampleExe) !void {
     const b = self.exe.step.owner;
-    const run_examples_cmd = b.addRunArtifact(self.exe);
-    run_examples_cmd.step.dependOn(b.getInstallStep());
+    const run_example_cmd = b.addRunArtifact(self.exe);
+    run_example_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
-        run_examples_cmd.addArgs(args);
+        run_example_cmd.addArgs(args);
     }
 
-    const step_name = try std.fmt.allocPrint(b.allocator, "example-{s}", .{self.name});
-    const step_description = try std.fmt.allocPrint(b.allocator, "Run example with name \"{s}\"", .{self.name});
+    const step_name = try std.fmt.allocPrint(b.allocator, "run-example-{s}", .{self.name});
+    const step_description = try std.fmt.allocPrint(b.allocator, "Run example in \"./examples/{s}/main.zig\"", .{self.name});
 
     const run_example_step = b.step(step_name, step_description);
-    run_example_step.dependOn(&run_examples_cmd.step);
+    run_example_step.dependOn(&run_example_cmd.step);
 }
 
-/// Adds the example to the build's check step
+/// Adds the example to the build's check step.
 pub fn addToCheckStep(self: *const ExampleExe, check_step: *std.Build.Step) void {
-    check_step.dependOn(&self.exe_check.step);
+    check_step.dependOn(&self.exe.step);
 }
 
-/// Adds the examples exe to the build's install target.
-pub fn install(self: *const ExampleExe) void {
-    const b = self.install_step.step.owner;
+/// Adds the example to the build's install step.
+pub fn addToInstallStep(self: *const ExampleExe) !void {
+    const b = self.exe.step.owner;
     b.installArtifact(self.exe);
 }
